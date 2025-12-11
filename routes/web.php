65e997da953b6015ff5049072d3885b45689bd7a
divta-suryawan <gitdivta@gmail.com>
2025-12-11 11:10:54 +0800

    create api kantor

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\CMS\LokasiKantorController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+
+/** route api */
+Route::prefix('presensi')->group(function () {
+
+    // lokasi kantor
+    Route::prefix('kantor')->controller(LokasiKantorController::class)->group(function () {
+        Route::get('/', 'getAllData');
+        Route::post('/create', 'createData');
+        Route::get('/get/{id}', 'getDataById');
+        Route::post('/update/{id}', 'updateData');
+        Route::delete('/delete/{id}', 'deleteData');
+    });
 });
