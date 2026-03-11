b0d8d1965bce15265926e575ceb390552e82d452
divta-suryawan <gitdivta@gmail.com>
2026-03-12 02:54:02 +0800

    update

--- routes/console.php
+++ routes/console.php
@@ -34,4 +34,4 @@ Schedule::call(function () {
     foreach ($lupaPulangUsers as $presensi) {
         AlphaNotificationJob::dispatch($presensi->id_user, $today, 'pulang');
     }
-})->everyMinute();
+})->dailyAt('23:00');
